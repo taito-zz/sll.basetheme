bbe36e309acd5389000ddccb279402b0bfdc3398
Taito Horiuchi <taito.horiuchi@gmail.com>
2012-12-12 10:34:47 +0900

    Release 0.3.4

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='sll.basetheme',
-    version='0.3.3',
+    version='0.3.4',
     description='Base theme for sll, slt and ll packages.',
     long_description=open("README.rst").read(),
     classifiers=[
