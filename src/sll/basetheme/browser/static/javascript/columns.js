1e62bf93f3802efe4c295c3a172d3d9f80d9cf34
Taito Horiuchi <taito.horiuchi@gmail.com>
2014-05-12 10:17:06 +0300

    fix

--- src/sll/basetheme/browser/static/javascript/columns.js
+++ src/sll/basetheme/browser/static/javascript/columns.js
@@ -2,12 +2,17 @@ jQuery(function() {
 
     var cwidth = $("#portal-columns").width();
 
-    if(cwidth > 970) {
+    if (cwidth > 970) {
         var wwidth = $(window).width();
         var side_margin = (wwidth - 970) / 2;
         var margin = '0 ' + side_margin + 'px';
         $('#portal-columns').css('width', '970px');
         $('#portal-columns').css('margin', margin);
+    }
+
+    if (cwidth <= 970) {
+        $('#portal-columns').css('width', '970px');
+        $('#portal-columns').css('margin', 'auto');
     }
 
     $(window).resize(function() {
@@ -21,6 +26,13 @@ jQuery(function() {
             $('#portal-columns').css('margin', margin);
         } else {
             $('#portal-columns').css('width', '100%');
+        }
+
+        if(wwidth >= 970) {
+            var side_margin = (wwidth - 970) / 2;
+            var margin = '0 ' + side_margin + 'px';
+            $('#portal-columns').css('width', '970px');
+            $('#portal-columns').css('margin', margin);
         }
     });
 
